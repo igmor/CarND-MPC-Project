51a2a52920d8a56814ff0c72487fde609a0c72b9
Igor Morozov <igor@uber.com>
2017-11-01 01:48:15 -0700

    Change bounding conditions for delta to -25/25 degrees

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -191,8 +191,8 @@ vector<double> MPC::Solve(Eigen::VectorXd state, Eigen::VectorXd coeffs) {
   // degrees (values in radians).
   // NOTE: Feel free to change this to something else.
   for (int i = delta_start; i < a_start; i++) {
-      vars_lowerbound[i] = -1.0;
-      vars_upperbound[i] = 1.0;
+      vars_lowerbound[i] = -0.4363;
+      vars_upperbound[i] = 0.4363;
   }
 
   // Acceleration/decceleration upper and lower limits.
